0769306576abf3c8939611b90e6856430be49ab7
Lukasz Wojciechowski <l.wojciechow@partner.samsung.com>
2015-12-17 15:06:13 +0100

    Add missing include of stdlib.h for getnev(3) function
    Usage of getenv(3) function requires stdlib.h
    Proper header was added to src/dpl/log/src/log.cpp file.
    
    Change-Id: Ie9a33c71e17ebe2d1fb2bc7f47117b1cd75ae046

--- src/dpl/log/src/log.cpp
+++ src/dpl/log/src/log.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011 Samsung Electronics Co., Ltd All Rights Reserved
+ * Copyright (c) 2011-2015 Samsung Electronics Co., Ltd All Rights Reserved
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@
  * @brief       This file is the implementation file of log system
  */
 #include <stddef.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include <dpl/log/log.h>
